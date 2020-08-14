d659404aec3a83ca47e7f236948c8ad22ef5bd78
ahoho <ahoyle@wesleyan.edu>
2020-08-14 07:35:35 -0700

    evaluate generation of data-to-text

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -1,99 +1,145 @@
 import argparse
 import json
+import sys
 from pathlib import Path
+
+sys.path.append("..")
 
 import torch
 from tqdm import tqdm
+from transformers import AutoTokenizer, BertTokenizer, T5Tokenizer
 
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
+from finetune import SummarizationModule, TranslationModule, TranslationModule
 
 try:
-    from .utils import calculate_rouge, use_task_specific_params, calculate_bleu_score, trim_batch
+    from .utils import pickle_load, pickle_save, save_json, Seq2SeqDataset, calculate_bleu_score
 except ImportError:
-    from utils import calculate_rouge, use_task_specific_params, calculate_bleu_score, trim_batch
+    from utils import pickle_load, pickle_save, save_json, Seq2SeqDataset, calculate_bleu_score
 
 DEFAULT_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i : i + n]
-
-
-def generate_summaries_or_translations(
-    examples: list,
-    out_file: str,
-    model_name: str,
-    batch_size: int = 8,
-    device: str = DEFAULT_DEVICE,
-    fp16=False,
-    task="summarization",
+def generate_from_model(
+    data_loader,
+    model,
+    device=DEFAULT_DEVICE,
     **gen_kwargs,
 ) -> None:
-    fout = Path(out_file).open("w", encoding="utf-8")
-    model_name = str(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
-    if fp16:
-        model = model.half()
+    
+    all_preds = []
+    for batch in tqdm(data_loader):
+        pad_token_id = model.tokenizer.pad_token_id
+        source_ids, source_mask, y = Seq2SeqDataset.trim_seq2seq_batch(batch, pad_token_id)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+        generated_ids = model.model.generate(
+            input_ids=source_ids,
+            attention_mask=source_mask,
+            use_cache=True,
+            decoder_start_token_id=model.decoder_start_token_id,
+            **gen_kwargs,
+        )
+        preds = model.ids_to_clean_text(generated_ids)
+        all_preds.extend(preds)
+        # TODO: add loss to output?
+    
+    return all_preds
 
-    # update config with summarization specific params
-    use_task_specific_params(model, task)
+def val_tokenize(lines, tokenizer=None):
+    """
+    To ensure consistency with other models, we want to tokenize/detokenize in the same
+    way across all models.
 
-    for batch in tqdm(list(chunks(examples, batch_size))):
-        if "t5" in model_name:
-            batch = [model.config.prefix + text for text in batch]
-        batch = tokenizer(batch, return_tensors="pt", truncation=True, padding="max_length").to(device)
-        input_ids, attention_mask = trim_batch(**batch, pad_token_id=tokenizer.pad_token_id)
-        summaries = model.generate(input_ids=input_ids, attention_mask=attention_mask, **gen_kwargs)
-        dec = tokenizer.batch_decode(summaries, skip_special_tokens=True, clean_up_tokenization_spaces=False)
-        for hypothesis in dec:
-            fout.write(hypothesis + "\n")
-            fout.flush()
+    TODO: Fix this across all modules, since it's messy
+    """
+    if tokenizer is None:
+        return lines
+    
+    lines = [" ".join(tokenizer.tokenize(l)) for l in lines]
+    if isinstance(tokenizer, T5Tokenizer):
+        lines = [l.replace(" ", "").replace("▁", " ") for l in lines]
+    if isinstance(tokenizer, BertTokenizer):
+        lines = [l.replace(" ##", "") for l in lines]
+    return lines
 
 
 def run_generate():
     parser = argparse.ArgumentParser()
-    parser.add_argument("model_name", type=str, help="like facebook/bart-large-cnn,t5-base, etc.")
-    parser.add_argument("input_path", type=str, help="like cnn_dm/test.source")
-    parser.add_argument("save_path", type=str, help="where to save summaries")
+    parser.add_argument("model_dir", type=str, help="Directory containing model and saved args")
+    parser.add_argument("output_dir", type=str, help="where to save the outputs")
 
-    parser.add_argument("--reference_path", type=str, required=False, help="like cnn_dm/test_reference_summaries.txt")
-    parser.add_argument("--score_path", type=str, required=False, help="where to save the rouge score in json format")
+    parser.add_argument("--type_path", type=str, default="val", help="Split from which to generate, i.e. <type_path>.source")
+    parser.add_argument("--reference_paths", nargs="+", required=False, help="like val.target val-2.target")
+    parser.add_argument("--data_dir", type=str, help="Input location (default uses that in saved args)")
+    parser.add_argument("--tokenizer_path_or_name", default=None, help="Use this to match tokenization between models")
     parser.add_argument("--device", type=str, required=False, default=DEFAULT_DEVICE, help="cuda, cuda:1, cpu etc.")
-    parser.add_argument("--task", type=str, default="summarization", help="typically translation or summarization")
     parser.add_argument("--bs", type=int, default=8, required=False, help="batch size")
     parser.add_argument(
         "--n_obs", type=int, default=-1, required=False, help="How many observations. Defaults to all."
     )
-    parser.add_argument("--fp16", action="store_true")
-    args = parser.parse_args()
-    examples = [" " + x.rstrip() if "t5" in args.model_name else x.rstrip() for x in open(args.input_path).readlines()]
-    if args.n_obs > 0:
-        examples = examples[: args.n_obs]
 
-    generate_summaries_or_translations(
-        examples,
-        args.save_path,
-        args.model_name,
-        batch_size=args.bs,
-        device=args.device,
-        fp16=args.fp16,
-        task=args.task,
+    args = parser.parse_args()
+    
+    # Reset previously used arguments as necessary
+    prev_args = pickle_load(Path(args.model_dir, "hparams.pkl"))
+    prev_args.model_name_or_path = str(Path(prev_args.output_dir, "best_tfmr"))
+    setattr(prev_args, f"n_{args.type_path}", args.n_obs)
+
+    if args.data_dir is not None:
+        prev_args.data_dir = args.data_dir
+
+    if prev_args.task == "summarization":
+        model = SummarizationModule(prev_args)
+    elif prev_args.task == "translation":
+        model = TranslationModule(prev_args)
+    elif prev_args.task == "data-to-text":
+        model = TranslationModule(prev_args)
+
+    data_loader = model.get_dataloader(
+        type_path=args.type_path, batch_size=args.bs, shuffle=False
     )
-    if args.reference_path is None:
+
+    Path(args.output_dir).mkdir(exist_ok=True)
+
+    # Make generations
+    print("Generating...")
+    preds = generate_from_model(
+        data_loader=data_loader,
+        model=model,
+        output_dir=args.output_dir,
+        device=args.device,
+    )
+
+    pickle_save(args, Path(args.output_dir, f"{args.type_path}-hparams.pkl"))
+
+    with open(Path(args.output_dir, f"{args.type_path}.pred"), "w") as outfile:
+        for sent in preds:
+            outfile.write(f"{sent}\n")
+    
+    if not args.reference_paths:
         return
+    
     # Compute scores
-    score_fn = calculate_bleu_score if "translation" in args.task else calculate_rouge
-    output_lns = [x.rstrip() for x in open(args.save_path).readlines()]
-    reference_lns = [x.rstrip() for x in open(args.reference_path).readlines()][: len(output_lns)]
-    scores: dict = score_fn(output_lns, reference_lns)
-    if args.score_path is not None:
-        json.dump(scores, open(args.score_path, "w+"))
-    return scores
+    score_fn = calculate_bleu_score # TODO: support others
+
+    val_tokenizer = None
+    if args.tokenizer_path_or_name is not None:
+        val_tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path_or_name)
+
+    preds = val_tokenize(preds, val_tokenizer)
+
+    reference_lns = []
+    for path in args.reference_paths:
+        if not Path(path).exists():
+            # silently try in the input directory
+            path = Path(prev_args.data_dir, path)
+        
+        with open(path, "r") as infile:
+            lines = [x.strip() for i, x in enumerate(infile) if i < len(preds)]
+        lines = val_tokenize(lines, val_tokenizer)
+        reference_lns.append(lines)
+
+    scores = score_fn(preds, reference_lns)
+    save_json(scores, Path(args.output_dir, f"{args.type_path}-bleu.json"))
 
 
 if __name__ == "__main__":
