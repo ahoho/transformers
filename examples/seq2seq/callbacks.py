efacfcffc52adacf6aa993e8fb68d5e0883cdf28
ahoho <ahoyle@wesleyan.edu>
2020-10-17 12:55:40 -0700

    update callbacks to support loss as metric

--- examples/seq2seq/callbacks.py
+++ examples/seq2seq/callbacks.py
@@ -77,6 +77,8 @@ def get_checkpoint_callback(output_dir, metric):
         exp = "{val_avg_rouge2:.4f}-{step_count}"
     elif metric == "bleu":
         exp = "{val_avg_bleu:.4f}-{step_count}"
+    elif metric == "loss":
+        exp = "{val_avg_loss:.4f}-{step_count}"
     else:
         raise NotImplementedError(
             f"seq2seq callbacks only support rouge2 and bleu, got {metric}, You can make your own by adding to this function."
@@ -85,7 +87,7 @@ def get_checkpoint_callback(output_dir, metric):
     checkpoint_callback = ModelCheckpoint(
         filepath=os.path.join(output_dir, exp),
         monitor=f"val_{metric}",
-        mode="max",
+        mode="min" if metric == "loss" else "max",
         save_top_k=1,
         period=0,  # maybe save a checkpoint every time val is run, not just end of epoch.
     )
