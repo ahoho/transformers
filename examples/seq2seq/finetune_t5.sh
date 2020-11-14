5236085b9ec8f55dd6516346ef73e850837ee8eb
ahoho <ahoyle@wesleyan.edu>
2020-11-13 19:47:06 -0800

    update finetuning shell scripts

--- examples/seq2seq/finetune_t5.sh
+++ examples/seq2seq/finetune_t5.sh
@@ -8,6 +8,6 @@ python finetune.py \
 --eval_batch_size=$BS \
 --output_dir=$OUTPUT_DIR \
 --max_source_length=512 \
---val_check_interval=0.1 --n_val=200 \
+--val_check_interval=0.1 \
 --do_train --do_predict \
  $@
--- examples/seq2seq/finetune_t5_webnlg.sh
+++ examples/seq2seq/finetune_t5_webnlg.sh
@@ -2,22 +2,25 @@
 export PYTHONPATH="../":"${PYTHONPATH}"
 
 /home/alexanderh/miniconda3/envs/cgen-transformer/bin/python finetune.py \
---data_dir=../../../data/webnlg/linearized/complete-entities \
+--data_dir=../../../data/webnlg/linearized/complete-entities-demarcated \
 --learning_rate=0.001 \
---train_batch_size=128 \
---eval_batch_size=128 \
---output_dir=../../../outputs/t5-linearized_complete-default_gen_hp-beam_10-lr_0.001-bs_256 \
+--train_batch_size=32 \
+--eval_batch_size=32 \
+--output_dir=../../../models/webnlg/linearized-t5/baseline-large-lr_0.001 \
 --max_source_length=176 \
 --max_target_length=122 \
 --val_max_target_length=122 \
 --test_max_target_length=122 \
 --num_beams 10 \
---val_check_interval=1 --n_val=200 \
---do_train --do_predict \
---model_name_or_path t5-small \
+--val_check_interval=1 \
+--do_train \
+--model_name_or_path t5-large \
 --task data-to-text \
 --gpus=1 \
+--adafactor \
 --new_tokens_fpath=../../../added_tokens.json \
---max_steps=1000 \
---gradient_accumulation_steps=2 \
---max_epochs=5
+--max_steps=5000 \
+--gradient_accumulation_steps=8 \
+--shuffle_graph_components \
+--seed 11235
+#--shuffle_graph_subcomponents
